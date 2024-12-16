87bbeb964312157abb83f9f1081bf01368edab85
Andrés Cufari <andrescufari@Andress-MacBook-Pro-2.local>
2024-12-16 20:04:02 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-// src/index.js
 const { NetworkClient } = require('./networkClient');
 
 async function main() {
   console.log('Starting P2P LLM Client...');
   const client = new NetworkClient();
-  await client.connect();
+  await client.start().catch(console.error);
 }
 
-main().catch(console.error);
+main();
